379f04696fe1fd1f99745f312c924693570735ef
Prakhar1808 <Prakhar1808@users.noreply.github.com>
2026-08-06 19:05:50 +0530

    basics: add helloWorld and basics
    header files
    multiline comments
    printing
    return type for main

---
+++ basicsOfC++/helloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  // header file is included
+using namespace std; // defines namespace
+
+int main() { // 'return-type' main
+    /* multline comment wow
+    ikr */
+  cout << "Hello World";
+  return 0;
+}
