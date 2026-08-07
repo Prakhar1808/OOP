787ee9ddd5a9272f2a7126dedcfc78e3b885f67c
Prakhar1808 <Prakhar1808@users.noreply.github.com>
2026-08-07 09:37:54 +0530

    basics: sum of number, input, output

---
+++ basicsOfC++/sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter Two Numbers: ";
+    int a, b;
+    cin >> a >> b;
+
+    cout<<"Sum Of input: "<< a + b;
+    return 0;
+}
